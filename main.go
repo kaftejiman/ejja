fca1fa61049b47baa6a0645dc013fb416178f12d
kaftejiman <kaftejiman@protonmail.ch>
2021-02-22 10:06:10 -0800

    init

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+)
 
 func main() {
-	fmt.Printf("test")
+
+	src := []byte(`package main
+	var sum int = 3 + 2`)
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, "demo", src, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	ast.Fprint(os.Stdout, fset, node, nil)
 }
